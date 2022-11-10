212592c231a2049ce9f376fb74ff1a804c77e7d3
Lanakod <lanakod.official@gmail.com>
2022-11-10 10:13:17 +0300

    database connection & some config tweaks

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,11 @@ import (
 	"github.com/Lanakod/go-todo/pkg/handler"
 	"github.com/Lanakod/go-todo/pkg/repository"
 	"github.com/Lanakod/go-todo/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
@@ -14,7 +17,23 @@ func main() {
 		log.Fatalf("Error occured while initializing configs:\n%s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error occured while loading env variables:\n%s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("Error occured while initializing database:\n%s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
