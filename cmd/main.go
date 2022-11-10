9ecd87efe79b6af7962ce1b16e5179bd672cff6f
Lanakod <lanakod.official@gmail.com>
2022-11-10 08:19:24 +0300

    dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"github.com/Lanakod/go-todo"
+	"github.com/Lanakod/go-todo/pkg/handler"
+	"github.com/Lanakod/go-todo/pkg/repository"
+	"github.com/Lanakod/go-todo/pkg/service"
 	"log"
-	"todo"
-	"todo/pkg/handler"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server:\n%s", err.Error())
